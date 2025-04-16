593ffb613bc4309b3b5253a7e577da28d16ab0ed
brunomanzoli <professorbrunomanzoli@outlook.com>
2025-04-16 19:42:33 -0300

    segunda versao

--- AppTeste/AppTeste/Form1.cs
+++ AppTeste/AppTeste/Form1.cs
@@ -16,5 +16,10 @@ namespace AppTeste
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bem Vindo ao Sistema");
+        }
     }
 }
